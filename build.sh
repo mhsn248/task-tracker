2aaa22c411a19fc277887b16cbd2d701a8e405dd
mhsn248 <mhsneaddr@gmail.com>
2026-06-17 07:49:59 +0330

    update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 python manage.py migrate
-python manage.py create_admin
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='demo').exists() or User.objects.create_superuser('admin','demo@example.com','123')"
 python manage.py collectstatic --noinput
