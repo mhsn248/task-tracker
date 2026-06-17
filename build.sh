c671aafd8894f731f12b827bb60c36cd7131e8a7
mhsn248 <mhsneaddr@gmail.com>
2026-06-17 07:39:46 +0330

    ui: restrict navigation items for student users and simplify header

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ pip install -r requirements.txt
 python manage.py migrate
 
 python manage.py collectstatic --noinput
+
+python manage.py migrate
+python manage.py create_admin
+python manage.py collectstatic --noinput
